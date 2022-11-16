3f542b1d42e4323b80e5b000d0e7f13da2ac836d
shrinivasbimal <shrinivasbimal@localhost>
2022-11-16 17:10:13 -0600

    done

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,83 @@
 package studio8;
 
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Date {
+	int month;
+	int day;
+	int year;
+	boolean isHoliday;
+	/**
+	 * 
+	 * @param month
+	 * @param day
+	 * @param year
+	 * @param isHoliday
+	 */
+	public Date(int month,int day,int year,boolean isHoliday ) {
+		this.month= month;
+		this.day= day;
+		this.year= year;
+		this.isHoliday=isHoliday;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public boolean isHoliday() {
+		return isHoliday;
+	}
+	public void setHoliday(boolean isHoliday) {
+		this.isHoliday = isHoliday;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(day, month, year);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && month == other.month && year == other.year;
+	}
+	public String toString() {
+		return month+ " "+day+","+year;
+	}
 
-
-    public static void main(String[] args) {
-
-    }
+	public static void main(String[] args) {
+		Date date1 = new Date(1,2,2003,false);
+		Date date2 = new Date(1,3,2003,false);
+		Date date3 = new Date(1,5,2013,true);
+		Date date4 = new Date(1,7,2005,false);
+		Date date5 = new Date(1,10,2033,true);
+		LinkedList<Date> list = new LinkedList<Date>();
+		list.add(date1);
+		list.add(date2);
+		list.add(date3);
+		list.add(date4);
+		list.add(date5);
+		list.add(date5);
+		System.out.println(list);
+	}
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,95 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.Objects;
 
 public class Time {
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, minute);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && minute == other.minute;
+	}
+	int hour;
+	int minute;
+	boolean isMilitary;
+	/**
+	 * 
+	 * @param hour
+	 * @param minute
+	 * @param isMilitary
+	 */
+	public Time(int hour, int minute, boolean isMilitary) {
+		this.hour = hour;
+		this.minute = minute;
+		this.isMilitary = isMilitary;
+
+	}
+	
+	public int getHour() {
+		return hour;
+	}
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+	public int getMinute() {
+		return minute;
+	}
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
+	public boolean isMilitary() {
+		return isMilitary;
+	}
+	public void setMilitary(boolean isMilitary) {
+		this.isMilitary = isMilitary;
+	}
+	public String toString() {
+		
+		if(isMilitary) {
+			return hour+":"+minute;
+			
+		}
+		else {
+			if(hour==0) {
+				return 12+":"+minute+" am.";
+			}
+			else if(hour== 12) {
+				return 12+":"+minute+" pm.";
+			}
+			if(hour>=12) {
+				return this.hour-12+":"+minute+" pm.";
+			}
+			else {
+				return this.hour+":"+this.minute+"am";
+			}
+		}
+	}
 	public static void main(String[] args) {
- 
-    	
-    }
+		Time time1 = new Time(12,30,false);
+		Time time2 = new Time(9,30,true);
+		Time time3 = new Time(11,30,false);
+		Time time4 = new Time(12,55,false);
+		Time time5 = new Time(1,30,true);
+		HashSet<Time> set = new HashSet<Time>();
+		set.add(time1);
+		set.add(time2);
+		set.add(time3);
+		set.add(time4);
+		set.add(time5);
+		set.add(time5);
+		System.out.println(set);
+		
+	}
 
 }
